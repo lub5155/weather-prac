d7b0566e22e6dc461e52e70165467f812976aa3f
lub5155 <vtarun2004@gmail.com>
2025-11-17 14:22:31 +0530

    disp api data
    displyed data from api

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metri
       .then(data => {
          document.getElementById("name").textContent = data.name;
          document.getElementById("tempp").textContent = data.main.temp + "°C";
+         document.getElementById("huu").textContent=data.main.humidity+"%"
+         document.getElementById("wuu").textContent=data.wind.speed+"Km/h"
          let w = data.weather[0].main;
          let icon = "";
          if (w == "Rain") icon = "rain.png";
