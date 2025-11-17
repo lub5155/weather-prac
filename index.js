79a6901e0854e2fe75b28b98578ccfe2a4d83e00
lub5155 <vtarun2004@gmail.com>
2025-11-17 13:58:31 +0530

    js
    added js fetching data from an api

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function getWeather() {
+let city = document.getElementById("inpu").value;
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=97e81509f0a8e62994237002427e1a4b`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+         document.getElementById("name").textContent = data.name;
+         document.getElementById("tempp").textContent = data.main.temp + "°C";
+         let w = data.weather[0].main;
+         let icon = "";
+         if (w == "Rain") icon = "rain.png";
+         else if (w == "Clear") icon = "clear.png";
+         else if (w == "Drizzle") icon = "drizzle.png";
+         else if (w == "Clouds") icon = "clouds.png";
+         else if (w == "Snow") icon = "snow.png";
+         else if (w == "Mist" || w == "Fog" || w == "Haze") icon = "mist.png";
+         else icon = "default.png";
+         document.getElementById("imm").innerHTML = `<img src="${icon}" id="imgs">`;
+      })
+      .catch(err => console.log("Error:", err));
+    }
